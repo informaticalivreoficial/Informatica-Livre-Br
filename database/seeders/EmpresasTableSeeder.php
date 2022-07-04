13f2b8c3a292a8e6b2501b46e2ad823296eac346
renato-Aspire-E5-574 <remontanari@live.com>
2022-07-04 19:50:57 -0300

    seeder empresas create

--- database/seeders/EmpresasTableSeeder.php
+++ database/seeders/EmpresasTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class CidadeTableSeeder extends Seeder
+class EmpresasTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
