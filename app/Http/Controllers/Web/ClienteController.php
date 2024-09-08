97b88217313dc4ac31930a163dce77eae27dac60
informaticalivreoficial <renatomontanari@ymail.com>
2024-09-08 15:15:09 -0300

    fix faturas

--- app/Http/Controllers/Web/ClienteController.php
+++ app/Http/Controllers/Web/ClienteController.php
@@ -88,7 +88,7 @@ class ClienteController extends Controller
                     'price_cents' => str_replace(',', '', $fatura->valor)   
                 ];
             }
-            
+            dd($data);
             $array = array_merge($data, $items); 
             return $this->gerarBoleto($array); 
                   
