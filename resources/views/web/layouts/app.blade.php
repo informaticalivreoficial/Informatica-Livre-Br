468421251e9fcabaf892fe18a3535421c5b6a587
informaticalivreoficial <renatomontanari@ymail.com>
2026-06-15 19:58:53 -0300

    analitics include

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -135,6 +135,14 @@
             window.open(whatsappUrl, '_blank');
         }
     </script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-RV4W59RMBL"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
+    gtag('config', 'G-RV4W59RMBL');
+    </script>
 </body>
 </html>
